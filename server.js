4df9686e4064cb5ebbb47d833f5a412ee0065635
Mr-Lokeshvb <118039342+Mr-Lokeshvb@users.noreply.github.com>
2025-06-13 11:35:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ✅ Root route to serve index.html from cozastore
-//app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'public','index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','login.html'));
 
 
 // ✅ Connect to MongoDB Atlas
